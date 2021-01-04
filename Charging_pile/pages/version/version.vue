3ffdb9dcec6d8c58e399318c61f4e0d9004cacef
cocoa123 <347362296@qq.com>
2021-01-04 10:19:06 +0800

    联城联充-微信

---
+++ Charging_pile/pages/version/version.vue
@@ -0,0 +1,87 @@
+<template>
+	<view class="content">
+		<view class="update_title">{{version}}</view>
+		<rich-text :nodes="content"></rich-text>
+		<!-- <view @tap="toOtherApplets">123321123</view> -->
+	</view>
+</template>
+
+<script>
+	import utlis from '../../static/js/utlis.js'
+	export default {
+		data() {
+			return {
+				version: '',
+				content: ''
+			}
+		},
+		onShow() {
+			this.getVersion();
+		},
+		methods: {
+			//前往其他小程序
+			// toOtherApplets() {
+			// 	uni.navigateToMiniProgram({
+			// 		appId: 'wxd5b706797f401a0d',
+			// 		path: '',
+			// 		extraData: {
+			// 			'data1': 'test'
+			// 		},
+			// 		success(res) {
+			// 			// 打开成功
+			// 			console.log('success')
+			// 		}
+			// 	})
+			// },
+			// 获取版本信息
+			getVersion() {
+				let self = this;
+				let data = {
+					userSource: 2,
+					xmType: 1
+				}
+				uni.showLoading({
+					title: '加载中',
+					mask: true
+				})
+				utlis.http("POST", 'cdxmVersion', data, function(res) {
+					if (res.status == "success") {
+						self.version = res.version;
+						self.content = res.content;
+						uni.setStorageSync('version',res.version)
+						uni.hideLoading()
+					} else {
+						uni.hideLoading()
+					}
+				})
+			}
+		}
+	}
+</script>
+
+<style>
+	.content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		background: rgba(243, 243, 243, 1);
+		width: 100%;
+		height: 100vh;
+		overflow: hidden;
+		position: relative;
+	}
+
+	.update_title {
+		margin-top: -400rpx;
+		margin-bottom: 50rpx;
+	}
+
+	.update_content {
+		color: #666666;
+		margin-top: 6rpx;
+		margin-bottom: 6rpx;
+		word-break: break-all;
+		line-height: 50rpx;
+	}
+</style>
---
+++ Charging_pile/unpackage/dist/dev/mp-alipay/app.js
@@ -0,0 +1,4 @@
+my.specialMethods = {"pages/power/power":["showPowerDetail"]}
+require('./common/runtime.js')
+require('./common/vendor.js')
+require('./common/main.js')
